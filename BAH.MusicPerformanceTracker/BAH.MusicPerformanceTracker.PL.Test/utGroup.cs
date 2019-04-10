73840c2af511562c4560b4dae5397b81061d27f6
Hunter Siebers <700189486@fvtc.edu>
2019-04-10 12:39:52 -0500

    Added PL.Test and Performer, GroupMember, and Group

---
+++ BAH.MusicPerformanceTracker/BAH.MusicPerformanceTracker.PL.Test/utGroup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BAH.MusicPerformanceTracker.PL.Test
+{
+    [TestClass]
+    public class utGroup
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ BAH.MusicPerformanceTracker/BAH.MusicPerformanceTracker.PL.Test/utGroupMember.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BAH.MusicPerformanceTracker.PL.Test
+{
+    [TestClass]
+    public class utGroupMember
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
