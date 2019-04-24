ad78dafb2d949cebc36bc88b1f086465ee8989b0
Hunter Siebers <700189486@fvtc.edu>
2019-04-23 23:46:28 -0500

    Added more PL Tests

---
+++ BAH.MusicPerformanceTracker/BAH.MusicPerformanceTracker.PL.Test/utPerformance.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BAH.MusicPerformanceTracker.PL;
+using System.Linq;
+
+namespace BAH.MusicPerformanceTracker.PL.Test
+{
+    [TestClass]
+    public class utPerformance
+    {
+        [TestMethod]
+        public void LoadTest()
+        {
+            using (MusicEntities dc = new MusicEntities())
+            {
+                int expected = 3;
+
+                var performances = dc.tblPerformances;
+
+                int actual = performances.Count();
+
+                Assert.AreEqual(expected, actual);
+            }
+
+        }
+
+        [TestMethod]
+        public void InsertTest()
+        {
+            using (MusicEntities dc = new MusicEntities())
+            {
+                tblPerformance performance = new tblPerformance();
+                performance.Id = Guid.NewGuid();
+                performance.Name = "PL Test";
+                performance.PerformanceDate = DateTime.Now;
+                performance.Location = "PL Test";
+                performance.pdfPath = "PL Test";
+                performance.Description = "PL Test";
+
+                dc.tblPerformances.Add(performance);
+
+                dc.SaveChanges();
+
+                tblPerformance retrievedPerformance = dc.tblPerformances.FirstOrDefault(a => a.Name == "PL Test");
+
+                Assert.AreEqual(performance.Id, retrievedPerformance.Id);
+            }
+        }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            using (MusicEntities dc = new MusicEntities())
+            {
+                tblPerformance performance = dc.tblPerformances.FirstOrDefault(a => a.Name == "PL Test");
+
+                performance.Location = "PL Updated Test"; ;
+
+                dc.SaveChanges();
+
+                tblPerformance retrievedPerformance = dc.tblPerformances.FirstOrDefault(a => a.Name == "PL Test");
+
+                Assert.IsNotNull(retrievedPerformance);
+            }
+        }
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            using (MusicEntities dc = new MusicEntities())
+            {
+                tblPerformance performance = dc.tblPerformances.FirstOrDefault(a => a.Name == "PL Test");
+
+                dc.tblPerformances.Remove(performance);
+
+                dc.SaveChanges();
+
+                tblPerformance retrievedPerformance = dc.tblPerformances.FirstOrDefault(a => a.Name == "PL Test");
+
+                Assert.IsNull(retrievedPerformance);
+            }
+        }
+    }
+}
---
+++ BAH.MusicPerformanceTracker/BAH.MusicPerformanceTracker.PL.Test/utPerformancePiece.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using BAH.MusicPerformanceTracker.PL;
+
+namespace BAH.MusicPerformancePieceTracker.PL.Test
+{
+    [TestClass]
+    public class utPerformancePiecePiece
+    {
+        [TestMethod]
+        public void LoadTest()
+        {
+            using (MusicEntities dc = new MusicEntities())
+            {
+                int expected = 6;
+
+                var performancepieces = dc.tblPerformancePieces;
+
+                int actual = performancepieces.Count();
+
+                Assert.AreEqual(expected, actual);
+            }
+
+        }
+
+        [TestMethod]
+        public void InsertTest()
+        {
+            using (MusicEntities dc = new MusicEntities())
+            {
+                tblPerformancePiece performancepiece = new tblPerformancePiece();
+                performancepiece.Id = Guid.NewGuid();
+                performancepiece.DirectorId = dc.tblDirectors.FirstOrDefault(p => p.FirstName == "Broderick").Id;
+                performancepiece.GroupId = dc.tblGroups.FirstOrDefault(p => p.Name == "Jazz Ensemble").Id;
+                performancepiece.PerformanceId = dc.tblPerformances.FirstOrDefault(p => p.Name == "Jazz Ensemble").Id;
+                performancepiece.PieceId = dc.tblPieces.FirstOrDefault(p => p.Name == "Rock Music").Id;
+                performancepiece.Notes = "PL Test";
+                performancepiece.MP3Path = "PL Test";
+
+                dc.tblPerformancePieces.Add(performancepiece);
+
+                dc.SaveChanges();
+
+                tblPerformancePiece retrievedPerformancePiece = dc.tblPerformancePieces.FirstOrDefault(a => a.Notes == "PL Test");
+
+                Assert.AreEqual(performancepiece.Id, retrievedPerformancePiece.Id);
+            }
+        }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            using (MusicEntities dc = new MusicEntities())
+            {
+                tblPerformancePiece performancepiece = dc.tblPerformancePieces.FirstOrDefault(a => a.Notes == "PL Test");
+
+                performancepiece.MP3Path = "PL Updated Test";
+
+                dc.SaveChanges();
+
+                tblPerformancePiece retrievedPerformancePiece = dc.tblPerformancePieces.FirstOrDefault(a => a.Notes == "PL Test");
+
+                Assert.IsNotNull(retrievedPerformancePiece);
+            }
+        }
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            using (MusicEntities dc = new MusicEntities())
+            {
+                tblPerformancePiece performancepiece = dc.tblPerformancePieces.FirstOrDefault(a => a.Notes == "PL Test");
+
+                dc.tblPerformancePieces.Remove(performancepiece);
+
+                dc.SaveChanges();
+
+                tblPerformancePiece retrievedPerformancePiece = dc.tblPerformancePieces.FirstOrDefault(a => a.Notes == "PL Test");
+
+                Assert.IsNull(retrievedPerformancePiece);
+            }
+        }
+    }
+}
