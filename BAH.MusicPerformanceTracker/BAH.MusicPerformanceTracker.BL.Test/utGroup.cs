de3484ee283ef8db9e210d2ff7527f3564e27683
Hunter Siebers <700189486@fvtc.edu>
2019-05-01 12:13:30 -0500

    Completed BL and BL.Test

--- BAH.MusicPerformanceTracker/BAH.MusicPerformanceTracker.BL.Test/utGroup.cs
+++ BAH.MusicPerformanceTracker/BAH.MusicPerformanceTracker.BL.Test/utGroup.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BAH.MusicPerformanceTracker.BL;
 
 namespace BAH.MusicPerformanceTracker.BL.Test
 {
@@ -7,9 +9,68 @@ namespace BAH.MusicPerformanceTracker.BL.Test
     public class utGroup
     {
         [TestMethod]
-        public void TestMethod1()
+        public void LoadTest()
         {
+            GroupList groups = new GroupList();
+            groups.Load();
+            Assert.AreEqual(3, groups.Count);
+        }
 
+
+        [TestMethod]
+        public void InsertTest()
+        {
+            Group group = new Group();
+            group.Name = "BL Test";
+            group.Description = "BL Test";
+            group.FoundedDate = Convert.ToDateTime("05/01/3000");
+
+            int results = group.Insert();
+            Assert.IsTrue(results == 1);
+        }
+
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            Group group = new Group();
+            GroupList groups = new GroupList();
+            groups.Load();
+            group = groups.FirstOrDefault(c => c.Name == "BL Test");
+
+            group.Description = "BL Test Update";
+            int results = group.Update();
+
+            Assert.IsTrue(results == 1);
+        }
+
+
+        [TestMethod]
+        public void LoadById()
+        {
+            Group group = new Group();
+            GroupList groups = new GroupList();
+            groups.Load();
+            group = groups.FirstOrDefault(c => c.Name == "BL Test");
+
+            Group newGroup = new Group { Id = group.Id };
+            newGroup.LoadById();
+
+            Assert.AreEqual(group.Name, newGroup.Name);
+        }
+
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            Group group = new Group();
+            GroupList groups = new GroupList();
+            groups.Load();
+            group = groups.FirstOrDefault(c => c.Name == "BL Test");
+
+            int results = group.Delete();
+
+            Assert.IsTrue(results == 1);
         }
     }
 }
---
+++ BAH.MusicPerformanceTracker/BAH.MusicPerformanceTracker.BL.Test/utPerformancePiece.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BAH.MusicPerformanceTracker.BL;
+using System.Linq;
+
+namespace BAH.MusicPerformanceTracker.BL.Test
+{
+    [TestClass]
+    public class utPerformancePiece
+    {
+        [TestMethod]
+        public void LoadTest()
+        {
+            PerformancePieceList PerformancePieces = new PerformancePieceList();
+            PerformancePieces.Load();
+            Assert.AreEqual(6, PerformancePieces.Count);
+        }
+
+
+        [TestMethod]
+        public void InsertTest()
+        {
+            //Load up Guids
+            PieceList pieces = new PieceList();
+            Piece piece = new Piece();
+            pieces.Load();
+            piece = pieces.FirstOrDefault(p => p.Name == "Rock Music");
+
+            GroupList groups = new GroupList();
+            Group group = new Group();
+            groups.Load();
+            group = groups.FirstOrDefault(c => c.Name == "Jazz Ensemble");
+
+            PerformanceList performances = new PerformanceList();
+            Performance performance = new Performance();
+            performances.Load();
+            performance = performances.FirstOrDefault(c => c.Name == "Dream Concert");
+
+            DirectorList directors = new DirectorList();
+            Director director = new Director();
+            directors.Load();
+            director = directors.FirstOrDefault(c => c.FirstName == "Broderick");
+
+            PerformancePiece performancePiece = new PerformancePiece();
+            performancePiece.GroupId = group.Id;
+            performancePiece.PerformanceId = performance.Id;
+            performancePiece.DirectorId = director.Id;
+            performancePiece.PieceId = piece.Id;
+            performancePiece.Notes = "BL Test";
+            performancePiece.MP3Path = "BL Test";
+
+            int results = performancePiece.Insert();
+            Assert.IsTrue(results == 1);
+        }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            //Load up Guids
+            PieceList pieces = new PieceList();
+            Piece piece = new Piece();
+            pieces.Load();
+            piece = pieces.FirstOrDefault(p => p.Name == "Rock Music");
+
+            GroupList groups = new GroupList();
+            Group group = new Group();
+            groups.Load();
+            group = groups.FirstOrDefault(c => c.Name == "Jazz Ensemble");
+
+            PerformanceList performances = new PerformanceList();
+            Performance performance = new Performance();
+            performances.Load();
+            performance = performances.FirstOrDefault(c => c.Name == "Dream Concert");
+
+            DirectorList directors = new DirectorList();
+            Director director = new Director();
+            directors.Load();
+            director = directors.FirstOrDefault(c => c.FirstName == "Eric");
+
+            PerformancePiece performancePiece = new PerformancePiece();
+            PerformancePieceList performancePieces = new PerformancePieceList();
+            performancePieces.Load();
+            performancePiece = performancePieces.FirstOrDefault(p => p.GroupId == group.Id && p.PieceId == piece.Id && p.PerformanceId == performance.Id);
+            performancePiece.Notes = "BL Test";
+            performancePiece.MP3Path = "BL Test";
+
+            performancePiece.DirectorId = director.Id;
+            int results = performancePiece.Update();
+
+            Assert.IsTrue(results == 1);
+        }
+
+        [TestMethod]
+        public void LoadById()
+        {
+            //Load up Guids
+            PieceList pieces = new PieceList();
+            Piece piece = new Piece();
+            pieces.Load();
+            piece = pieces.FirstOrDefault(p => p.Name == "Rock Music");
+
+            GroupList groups = new GroupList();
+            Group group = new Group();
+            groups.Load();
+            group = groups.FirstOrDefault(c => c.Name == "Jazz Ensemble");
+
+            PerformanceList performances = new PerformanceList();
+            Performance performance = new Performance();
+            performances.Load();
+            performance = performances.FirstOrDefault(c => c.Name == "Dream Concert");
+
+            DirectorList directors = new DirectorList();
+            Director director = new Director();
+            directors.Load();
+            director = directors.FirstOrDefault(c => c.FirstName == "Eric");
+
+            PerformancePiece performancePiece = new PerformancePiece();
+            PerformancePieceList performancePieces = new PerformancePieceList();
+            performancePieces.Load();
+            performancePiece = performancePieces.FirstOrDefault(p => p.GroupId == group.Id && p.PieceId == piece.Id && p.PerformanceId == performance.Id);
+            performancePiece.Notes = "BL Test";
+            performancePiece.MP3Path = "BL Test";
+
+            PerformancePiece newperformancePiece = new PerformancePiece { Id = performancePiece.Id };
+            newperformancePiece.LoadById();
+
+            Assert.AreEqual(performancePiece.DirectorId, newperformancePiece.DirectorId);
+        }
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            //Load up Guids
+            PieceList pieces = new PieceList();
+            Piece piece = new Piece();
+            pieces.Load();
+            piece = pieces.FirstOrDefault(p => p.Name == "Rock Music");
+
+            GroupList groups = new GroupList();
+            Group group = new Group();
+            groups.Load();
+            group = groups.FirstOrDefault(c => c.Name == "Jazz Ensemble");
+
+            PerformanceList performances = new PerformanceList();
+            Performance performance = new Performance();
+            performances.Load();
+            performance = performances.FirstOrDefault(c => c.Name == "Dream Concert");
+
+            DirectorList directors = new DirectorList();
+            Director director = new Director();
+            directors.Load();
+            director = directors.FirstOrDefault(c => c.FirstName == "Eric");
+
+            PerformancePiece performancePiece = new PerformancePiece();
+            PerformancePieceList performancePieces = new PerformancePieceList();
+            performancePieces.Load();
+            performancePiece = performancePieces.FirstOrDefault(p => p.GroupId == group.Id && p.PieceId == piece.Id && p.PerformanceId == performance.Id);
+            performancePiece.Notes = "BL Test";
+            performancePiece.MP3Path = "BL Test";
+
+            int results = performancePiece.Delete();
+
+            Assert.IsTrue(results == 1);
+        }
+    }
+}
