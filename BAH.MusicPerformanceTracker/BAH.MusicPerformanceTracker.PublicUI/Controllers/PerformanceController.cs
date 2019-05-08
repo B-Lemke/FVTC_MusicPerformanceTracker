26b7d6fccf52e048c9014e1afe7018ec62930d02
Brody Lemke <500161066@fvtc.edu>
2019-05-07 21:43:42 -0500

    Public UI updates and creation. Updated bootstrap.

---
+++ BAH.MusicPerformanceTracker/BAH.MusicPerformanceTracker.PublicUI/Controllers/PerformanceController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using BAH.MusicPerformanceTracker.BL;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace BAH.MusicPerformanceTracker.PublicUI.Controllers
+{
+    public class PerformanceController : Controller
+    {
+        PerformanceList performances;
+
+        // GET: Performance
+        public ActionResult Index()
+        {
+            performances = new PerformanceList();
+
+            //Initialize Cient
+            HttpClient client = InitializeClient();
+
+            //Call the API
+            HttpResponseMessage response = client.GetAsync("Performance").Result;
+
+            //Deserialize the json
+            string result = response.Content.ReadAsStringAsync().Result;
+            dynamic items = (JArray)JsonConvert.DeserializeObject(result);
+            performances = items.ToObject<PerformanceList>();
+
+            return View(performances);
+        }
+
+        // GET: Performance/Details/5
+        public ActionResult Details(Guid id)
+        {
+            Performance performance;
+
+            //Initialize Cient
+            HttpClient client = InitializeClient();
+
+            //Call the API
+            HttpResponseMessage response = client.GetAsync("Performance/" + id).Result;
+
+            //Deserialize the json
+            string result = response.Content.ReadAsStringAsync().Result;
+            performance = JsonConvert.DeserializeObject<Performance>(result);
+
+            return View(performance);
+        }
+
+
+
+        private static HttpClient InitializeClient()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("x-apikey", "12345");
+            client.BaseAddress = new Uri("http://musicperformancetracker.azurewebsites.net/api/");
+            return client;
+        }
+    }
+}
---
+++ BAH.MusicPerformanceTracker/BAH.MusicPerformanceTracker.PublicUI/Controllers/PieceController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using BAH.MusicPerformanceTracker.BL;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace BAH.MusicPieceTracker.PublicUI.Controllers
+{
+    public class PieceController : Controller
+    {
+        PieceList pieces;
+
+        // GET: Piece
+        public ActionResult Index()
+        {
+            pieces = new PieceList();
+
+            //Initialize Cient
+            HttpClient client = InitializeClient();
+
+            //Call the API
+            HttpResponseMessage response = client.GetAsync("Piece").Result;
+
+            //Deserialize the json
+            string result = response.Content.ReadAsStringAsync().Result;
+            dynamic items = (JArray)JsonConvert.DeserializeObject(result);
+            pieces = items.ToObject<PieceList>();
+
+            return View(pieces);
+        }
+
+        // GET: Piece/Details/5
+        public ActionResult Details(Guid id)
+        {
+            Piece piece;
+
+            //Initialize Cient
+            HttpClient client = InitializeClient();
+
+            //Call the API
+            HttpResponseMessage response = client.GetAsync("Piece/" + id).Result;
+
+            //Deserialize the json
+            string result = response.Content.ReadAsStringAsync().Result;
+            piece = JsonConvert.DeserializeObject<Piece>(result);
+
+            return View(piece);
+        }
+
+        private static HttpClient InitializeClient()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("x-apikey", "12345");
+            client.BaseAddress = new Uri("http://musicperformancetracker.azurewebsites.net/api/");
+            return client;
+        }
+
+    }
+}
